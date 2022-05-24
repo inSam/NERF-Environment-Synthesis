2ead31f91af10942893af3726bd0af33e132fce5
Sam Xu <samx@stanford.edu>
2022-05-24 11:02:53 -0700

    Pathing for camera

--- pbrt_nerf_db.py
+++ pbrt_nerf_db.py
@@ -1,4 +1,8 @@
+import re
+import math
+import tempfile
 from dataclasses import dataclass, asdict
+
 
 import numpy as np
 import cv2
@@ -26,8 +30,47 @@ class CameraInfo:
     p1: float = 0
     p2: float = 0
 
-def bistro_cafe_build(x, y, z, u, v, w, outfile):
-    """Construct pbrt file given a certain camera position and target in the reference coordinate system 
+def get_camera_info(infile: str) -> CameraInfo:
+    """Generates the camera information from the pbrt tempalte file
+
+    Args:
+        infile (str): Path to the pbrt template file
+
+    Returns:
+        CameraInfo: Data class containing camera information
+    """
+    height = 0
+    width = 0
+    fov = 0 # In degrees
+    with open(infile, 'r') as template_f:
+        for line in template_f:
+            if "xresolution" in line:
+                m = re.search(r'\[\s?(\d+)\s?\]', line)
+                if m != None:
+                    width = int(m.group(1))
+            if "yresolution" in line:
+                m = re.search(r'\[\s?(\d+)\s?\]', line)
+                if m != None:
+                    height = int(m.group(1))
+            if "float fov" in line:
+                m = re.search(r'\[\s?(\d+)\s?\]', line)
+                if m != None:
+                    fov = float(m.group(1))
+    
+    assert(height != 0 and width != 0)
+    assert(fov != 0)
+    
+    # convert to radians     
+    fov = math.radians(fov)
+    fl = min(height, width) / (2 * math.tan(fov/2))
+    fov_greater = 2 * math.atan(max(height, width) / (fl * 2)) * 2
+    if width > height:
+        return CameraInfo(fov_greater, fov, fl, fl, width, height)
+    else:
+        return CameraInfo(fov, fov_greater, fl, fl, width, height)
+    
+def render_pbrt(x, y, z, u, v, w, infile, outimg) -> None:
+    """Render pbrt file given a certain camera position and target in the reference coordinate system 
         1. Outputs the pbrt file
         2. Constructs the camera info class
 
@@ -40,18 +83,36 @@ def bistro_cafe_build(x, y, z, u, v, w, outfile):
         w (float): z pos of target position
         outfile (str): name of the output file
     """
-    height = 0
-    width = 0
-    fov = 0
+    eye_pos = " ".join(str(i) for i in [x, y, z])
+    look_pos = " ".join(str(i) for i in [u, v, w])
+
+    with tempfile.TemporaryFile('w') as tmp_fp:
+        with open(infile, 'r') as template_f:
+            # Generate pbrt file with correct camera params
+            for line in template_f:
+                line = line.replace("[CAMERA_POS]", eye_pos)
+                line = line.replace("[CAMERA_TGT]", look_pos)
+                line = line.replace("[OUTFILE]", '"' + outimg + '"')
+                tmp_fp.write(line)
+            # Render the pbrt file
+
+
+def pathgen_bistro()-> tuple[float, float, float, float, float, float]: 
+    eye_x, eye_y, eye_z = -11.7, 2.5, 6.5
+    tgt_x, tgt_y, tgt_z = -8.5, 2.8, 8
     
     
+    return (1,1,1,1,1,1)
 
 def main():
     PBRT_PATH = ""
     OUT_PATH = "./transform.json"
-    x = CameraInfo(45., 45., 10., 10.)
+
+    cam_info = get_camera_info("bistro_cafe_template.pbrt")
+
+    #build_pbrt(0,0,0,0,0,0, "bistro_cafe_template.pbrt")
         
-    out_json = asdict(x)
+    out_json = asdict(cam_info)
     with open(OUT_PATH, "w") as outfile:
         json.dump(out_json, outfile, indent=2)
 
