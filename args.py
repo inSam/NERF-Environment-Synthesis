a7dff086f6f8e1310cc5708ccb3b33288c9f5f66
Sam Xu <samx@stanford.edu>
2022-06-04 06:59:27 -0700

    major bug fixes

--- args.py
+++ args.py
@@ -7,10 +7,12 @@ def parse_args():
 
     parser.add_argument("--template", type=str, default="/home/samx/pbrt-v4-scenes/bistro/bistro_cafe_template.pbrt",
                         help="input path to the pbrt template")
-    parser.add_argument("--aabb_scale", default=16, choices=["1", "2", "4", "8", "16"],
+    parser.add_argument("--aabb_scale", default=4, choices=["1", "2", "4", "8", "16"],
                         help="large scene scale factor. 1 scene fits in unit cube; power of 2 up to 16")
     parser.add_argument("--num_samples", type=int, default=128,
                         help="Numbers of sample to generate")
+    parser.add_argument("--db", type=str, default="crown",
+                        choices=["bistro", "crown"], help="Decide which pbrt template to render")
     parser.add_argument("--out", default="./", help="output path")
     args = parser.parse_args()
     return args
--- pbrt_nerf_db.py
+++ pbrt_nerf_db.py
@@ -8,6 +8,56 @@ import numpy as np
 import cv2
 import json
 
+template_keys = {
+    "bistro": "/home/samx/pbrt-v4-scenes/bistro/bistro_cafe_template.pbrt",
+    "crown": "/home/samx/pbrt-v4-scenes/crown/crown_template.pbrt"
+}
+
+
+def generate_transform_matrix(pos, rot):
+    def Rx(theta):
+        return np.matrix([[1, 0, 0],
+                          [0, np.cos(theta), -np.sin(theta)],
+                          [0, np.sin(theta), np.cos(theta)]])
+
+    def Ry(theta):
+        return np.matrix([[np.cos(theta), 0, np.sin(theta)],
+                          [0, 1, 0],
+                          [-np.sin(theta), 0, np.cos(theta)]])
+
+    def Rz(theta):
+        return np.matrix([[np.cos(theta), -np.sin(theta), 0],
+                          [np.sin(theta), np.cos(theta), 0],
+                          [0, 0, 1]])
+
+    R = Rz(rot[2]) * Ry(rot[1]) * Rx(rot[0])
+    xf_rot = np.eye(4)
+    xf_rot[:3, :3] = R
+
+    xf_pos = np.eye(4)
+    xf_pos[:3, 3] = pos  # - average_position
+
+    # barbershop_mirros_hd_dense:
+    # - camera plane is y+z plane, meaning: constant x-values
+    # - cameras look to +x
+
+    # Don't ask me...
+    extra_xf = np.matrix([
+        [-1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1]])
+    # NerF will cycle forward, so lets cycle backward.
+    shift_coords = np.matrix([
+        [0, 0, 1, 0],
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1]])
+    xf = shift_coords @ extra_xf @ xf_pos
+    assert np.abs(np.linalg.det(xf) - 1.0) < 1e-4
+    xf = xf @ xf_rot
+    return xf
+
 
 def sharpness(imagePath):
     image = cv2.imread(imagePath, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
@@ -71,7 +121,7 @@ class CameraInfo:
     w: int
     h: int
     up_vec: np.ndarray
-    aabb_scale: int = 16
+    aabb_scale: int = 4
     k1: float = 0
     k2: float = 0
     p1: float = 0
@@ -125,7 +175,7 @@ def get_camera_info(infile: str) -> CameraInfo:
 
     # Calculate the FOV for both the X and Y direction
     fl = min(height, width) / (2 * math.tan(fov / 2))
-    fov_greater = 2 * math.atan(max(height, width) / (fl * 2)) * 2
+    fov_greater = 2 * math.atan(max(height, width) / (fl * 2))
     if width > height:
         return CameraInfo(fov_greater, fov, fl, fl, width, height, up_vec)
     else:
@@ -166,9 +216,9 @@ def render_pbrt(eye_pos, center_pos, infile, outimg) -> None:
 
 
 def pathgen_bistro(outfile: str) -> PBRT_Frame:
-    eye_x, eye_y, eye_z = [-11.7 + np.random.uniform(-2, 2),
-                           2.5 + np.random.uniform(-2, 2),
-                           6.5 + np.random.uniform(-2, 2)]
+    eye_x, eye_y, eye_z = [-11.7 + np.random.normal(-1, 1),
+                           2.5 + np.random.normal(-1, 1),
+                           6.5 + np.random.normal(-1, 1)]
     # eye_x, eye_y, eye_z = [-11.7,
     #                        2.5,
     #                        6.5]
@@ -184,39 +234,98 @@ def pathgen_bistro(outfile: str) -> PBRT_Frame:
                       outfile)
 
 
+def pathgen_crown(outfile: str) -> PBRT_Frame:
+    eye_to_dist_scale = 34.4419802
+
+    tgt_x, tgt_y, tgt_z = [0, 11, -10]
+    eye = np.random.normal(size=3)
+    eye[1] = max(eye[1], -0.01)
+    eye_x, eye_y, eye_z = normalize_vector(
+        eye) * eye_to_dist_scale + [tgt_x, tgt_y, tgt_z]
+    #eye_x, eye_y, eye_z = [0, 5.5, 24]
+
+    return PBRT_Frame(np.array([eye_x, eye_y, eye_z]),
+                      np.array([tgt_x, tgt_y, tgt_z]),
+                      outfile)
+
+
 def main():
     args = parse_args()
-    TEMPLATE_PATH = args.template
-    OUT_DIR = args.out
+    TEMPLATE_PATH = template_keys[args.db]
+
+    # Construct the Output directory
+    OUT_DIR = os.path.join(args.out, "images", args.db)
+    # Check to make sure that the output directory eexists
+    if not os.path.exists(OUT_DIR):
+        os.mkdir(OUT_DIR)
     assert(os.path.isfile(TEMPLATE_PATH))
     assert(os.path.isdir(OUT_DIR))
 
     cam_info = get_camera_info(TEMPLATE_PATH)
-    # Create Rotation matrix to adjust up vector to [0, 0, 1]
-    R = rotmat(cam_info.up_vec, [0, 0, 1])
-    R = np.pad(R, [0, 1])
-    R[-1, -1] = 1
+
+    # print(cam_info.up_vec)
+    # print(R)
+    #print(R @ np.array([0,1,0,1]))
+    # return
 
     frames = []
+    up = np.zeros(3)
     for i in range(args.num_samples):
-        frame = pathgen_bistro(os.path.join("images", f"{i:0>3d}.exr"))
+        out = os.path.join(OUT_DIR, f"{i:0>3d}.exr")
+
+        frame = None
+        if args.db == "bistro":
+            frame = pathgen_bistro(out)
+        elif args.db == "crown":
+            frame = pathgen_crown(out)
+        else:
+            raise Exception("Template not supported")
+
         render_pbrt(frame.eye_pos, frame.center_pos,
                     TEMPLATE_PATH, frame.filename)
 
+        #frame.eye_pos = (R @ np.append(frame.eye_pos, 1))[:3]
+        #frame.center_pos = (R @ np.append(frame.center_pos, 1))[:3]
+
+        # frame.eye_pos = frame.eye_pos[[0, 2, 1]]
+        # frame.center_pos = frame.center_pos[[0, 2, 1]]
         w2c = get_lookat_matrix(
-            frame.eye_pos, frame.center_pos, cam_info.up_vec)
+            np.array(frame.eye_pos), np.array(frame.center_pos), np.array([0, 1, 0]))
         c2w = np.linalg.inv(w2c)
+        #c2w = R @ c2w
+        # c2w[0:3,2] *= -1 # flip the y and z axis
+        # c2w[0:3,1] *= -1
+        # c2w = c2w[[1,0,2,3],:] # swap y and z
+        # c2w[2,:] *= -1 # flip whole world upside down
+        up += c2w[0:3,1]
         # sharp = sharpness(frame.filename)
         frames.append({
-            "file_path": frame.filename,
+            "file_path": f"./{i:0>3d}.exr",
             # "sharpness": sharp,
-            "transform_matrix": np.matmul(R, c2w).tolist()
+            "transform_matrix": c2w
+            # "transform_matrix": generate_transform_matrix([0,0,1], [0, 0, 0]).tolist()
+            # "transform_matrix": np.eye(4).tolist()
         })
+
+    up = up / np.linalg.norm(up)
+    print("up vector was", up)
+    # Create Rotation matrix to adjust up vector to [0, 0, 1]
+    R = rotmat([0,1,0], [0, 0, 1])
+    R = np.pad(R, [0, 1])
+    R[-1, -1] = 1
+    avglen = 0.
+    for f in frames:
+        f["transform_matrix"] = np.matmul(R, f["transform_matrix"])
+        avglen += np.linalg.norm(f["transform_matrix"][0:3, 3])
+    avglen /= len(frames)
+    for f in frames:
+        f["transform_matrix"][0:3, 3] *= 2.0 / avglen
+        f["transform_matrix"] = f["transform_matrix"].tolist()
 
     out_json = asdict(cam_info)
     del out_json["up_vec"]
     out_json["frames"] = frames
-    with open(os.path.join(OUT_DIR, "transform.json"), "w") as outfile:
+    with open(os.path.join(OUT_DIR, args.db + ".json"), "w") as outfile:
         json.dump(out_json, outfile, indent=2)
 
 
